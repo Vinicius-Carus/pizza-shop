60fb2e033e7fa653a07a3d24b8134770368e10f8
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-04 16:52:23 -0300

    feat: add singin ui and change the shadcn theme

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
-
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 import Dashboard from "./pages/app/dashboard";
-import SignIn from "./pages/auth/sigin";
+import SignIn from "./pages/auth/signin";
 import AppLayout from "./pages/_layouts/app";
 import AuthLayout from "./pages/_layouts/auth";
 
@@ -13,6 +13,6 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AuthLayout />,
-    children: [{ path: "/sigin", element: <SignIn /> }],
+    children: [{ path: "/signin", element: <SignIn /> }],
   },
 ]);
