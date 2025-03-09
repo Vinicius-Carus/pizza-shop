6012c47f93e58991ae119104299ef0656f9eb87b
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-09 11:02:26 -0300

    feat: popular products chart ui in dashboard

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -6,7 +6,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { ResponsiveContainer, LineChart, XAxis, YAxis, Line } from "recharts";
+import {
+  ResponsiveContainer,
+  LineChart,
+  XAxis,
+  YAxis,
+  Line,
+  CartesianGrid,
+} from "recharts";
 
 import colors from "tailwindcss/colors";
 
@@ -47,6 +54,9 @@ export default function RevenueChart() {
                 })
               }
             />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
