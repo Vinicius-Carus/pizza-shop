2edf049d5df8b41773deeaab79b78fea1d251de1
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-09 10:46:33 -0300

    feat: add revenue chart in dashboard page

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,7 @@ import SignIn from "./pages/auth/sign-in";
 import AppLayout from "./pages/_layouts/app";
 import AuthLayout from "./pages/_layouts/auth";
 import SignUp from "./pages/auth/sign-up";
-import Orders from "./pages/orders/orders";
+import Orders from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
