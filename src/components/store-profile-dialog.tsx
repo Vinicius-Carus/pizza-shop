c066bd4e8d5bca4d8af32428c6d90289b2d5320d
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-18 07:43:35 -0300

    feat: implement profile update functionality and form handling

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,5 +1,6 @@
 import { DialogTitle } from "@radix-ui/react-dialog";
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -7,13 +8,15 @@ import {
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
-import { Input } from "./ui/input";
-import { Textarea } from "./ui/textarea";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { updateProfile } from "@/api/update-profile";
+import { toast } from "sonner";
+import { InputHookForm } from "./ui/input-hook-form";
+import { TextareaHookForm } from "./ui/textarea-hook-form";
 
 const storeProfileSchema = z.object({
   name: z.string().min(1),
@@ -26,15 +29,38 @@ export default function StoreProfileDialog() {
   const { data: managedRestaurant } = useQuery({
     queryKey: ["managed-restaurant"],
     queryFn: getManagedRestaurant,
+    staleTime: Infinity,
   });
 
-  const { register, handleSubmit } = useForm({
+  const storeProfileFormData = useForm({
     resolver: zodResolver(storeProfileSchema),
     values: {
       name: managedRestaurant?.name ?? "",
       description: managedRestaurant?.description ?? "",
     },
   });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = storeProfileFormData;
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+  });
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description,
+      });
+
+      toast.success("Perfil atualizado com sucesso");
+    } catch {
+      toast.error("Falha ao atualizar o perfil, tente novamente");
+    }
+  }
 
   return (
     <DialogContent>
@@ -44,35 +70,39 @@ export default function StoreProfileDialog() {
           Atualize as informações do seu estabelecimento visiveis ao seu cliente
         </DialogDescription>
       </DialogHeader>
+      <FormProvider {...storeProfileFormData}>
+        <form onSubmit={handleSubmit(handleUpdateProfile)}>
+          <div className="space-y-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label className="text-right" htmlFor="name">
+                Nome
+              </Label>
+              <InputHookForm name="name" className="col-span-3" id="name" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label className="text-right" htmlFor="description">
+                Nome
+              </Label>
+              <TextareaHookForm
+                className="col-span-3"
+                id="description"
+                name="description"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="ghost" type="button">
+                Cancelar
+              </Button>
+            </DialogClose>
 
-      <form action="">
-        <div className="space-y-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label className="text-right" htmlFor="name">
-              Nome
-            </Label>
-            <Input className="col-span-3" id="name" {...register("name")} />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label className="text-right" htmlFor="description">
-              Nome
-            </Label>
-            <Textarea
-              className="col-span-3"
-              id="description"
-              {...register("description")}
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button variant="ghost" type="button">
-            Cancelar
-          </Button>
-          <Button type="submit" variant="success">
-            Salvar
-          </Button>
-        </DialogFooter>
-      </form>
+            <Button type="submit" variant="success" disabled={isSubmitting}>
+              Salvar
+            </Button>
+          </DialogFooter>
+        </form>
+      </FormProvider>
     </DialogContent>
   );
 }
