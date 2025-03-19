d8f6e23c169f499731dc7f8a92c4e69c88c03ea4
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-18 21:03:35 -0300

    feat: add order management functionality with order retrieval and status display

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,4 +1,3 @@
-import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -10,8 +9,15 @@ import { Helmet } from "react-helmet-async";
 import OrderTableRow from "./order-table-row";
 import OrderTableFilters from "./order-table-filters";
 import Pagination from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export default function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  });
+
   return (
     <>
       <Helmet title="Pedidos" />
@@ -36,9 +42,10 @@ export default function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => (
-                  <OrderTableRow key={i} />
-                ))}
+                {result &&
+                  result.orders.map((order) => (
+                    <OrderTableRow key={order.orderId} order={order} />
+                  ))}
               </TableBody>
             </Table>
           </div>
