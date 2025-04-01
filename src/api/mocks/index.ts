31dc94578c45f32f1f28cec8144d419223da95ac
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-03-31 21:52:48 -0300

    feat: update API response interfaces to export and add mock data for profile and restaurant

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -6,6 +6,11 @@ import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
 import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
 import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-amount-mock";
 import { getMonthRevenueMock } from "./get-month-revenue";
+import { getProfileMock } from "./get-profile-mock";
+import { updateProfileMock } from "./update-profile-mock";
+import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
+import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period";
+import { getPopularProductsMock } from "./get-popular-products-month-mock";
 
 export const worker = setupWorker(
   signInMock,
@@ -14,6 +19,11 @@ export const worker = setupWorker(
   getMonthOrdersAmountMock,
   getMonthCanceledOrdersAmountMock,
   getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+  getProfileMock,
+  updateProfileMock,
+  getManagedRestaurantMock,
 );
 
 export async function enableMSW() {
