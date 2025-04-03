48371b5ef663f63d2ee7ef31ed0597fee4308f42
Vinicius-Carus <viniciuscaruss@gmail.com>
2025-04-03 20:52:25 -0300

    feat: implement sign-up functionality and add end-to-end tests for registration

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,60 @@
+import { test, expect } from "@playwright/test";
+
+test("sign-up successfully", async ({ page }) => {
+  await page.goto("/sign-up", { waitUntil: "networkidle" });
+
+  await page
+    .getByRole("textbox", { name: "Nome do estabelecimento" })
+    .fill("Pizza shop");
+
+  await page.getByRole("textbox", { name: "Seu nome" }).fill("John doe");
+
+  await page
+    .getByRole("textbox", { name: "Seu e-mail" })
+    .fill("pizzashop@test.com");
+
+  await page.getByRole("textbox", { name: "Seu celular" }).fill("123213323123");
+
+  await page.getByRole("button", { name: "Finalizar cadastro" }).click();
+
+  const toast = page.getByText("Restaurante cadastrado com sucesso!");
+
+  expect(toast).toBeVisible();
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  const emailSignIn = page.getByRole("textbox", { name: "Seu e-mail" });
+
+  expect(page.url()).toContain("/sign-in");
+  expect(emailSignIn).toHaveValue("pizzashop@test.com");
+});
+
+test("sign-up with error", async ({ page }) => {
+  await page.goto("/sign-up", { waitUntil: "networkidle" });
+
+  await page
+    .getByRole("textbox", { name: "Nome do estabelecimento" })
+    .fill("Invalid name");
+
+  await page.getByRole("textbox", { name: "Seu nome" }).fill("Invalid name");
+
+  await page
+    .getByRole("textbox", { name: "Seu e-mail" })
+    .fill("invalid@test.com");
+
+  await page.getByRole("textbox", { name: "Seu celular" }).fill("2313433123");
+
+  await page.getByRole("button", { name: "Finalizar cadastro" }).click();
+
+  const toast = page.getByText("Erro ao cadastrar restaurante");
+
+  expect(toast).toBeVisible();
+});
+
+test("navigate to sign-in", async ({ page }) => {
+  await page.goto("/sign-up", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Fazer login" }).click();
+
+  expect(page.url()).toContain("/sign-in");
+});
